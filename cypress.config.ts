b0aa7b46fc1e9ff3fbeb815f97f8eaceb524d7b1
Donia <donia@aouali.net>
2025-10-22 23:27:35 +0200

    fix failing tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
       bundler: "webpack",
     },
     specPattern: "src/app/shared/tests/unit/**/*.cy.ts",
-    testIsolation: false,
   },
 
   e2e: {
@@ -15,6 +14,5 @@ export default defineConfig({
       // implement node event listeners here
     },
     specPattern: "src/app/shared/tests/e2e/**/*.cy.ts",
-    testIsolation: false,
   },
 });
